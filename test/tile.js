79d38a0c92f042e8f2e361bf2b8f3097d84fbac4
Blixa Morgan <blixa@projectmakeit.com>
2014-01-21 19:38:14 +0000

    add check for tile creationwq

--- test/tile.js
+++ test/tile.js
@@ -3,27 +3,34 @@ var tile = require("../build/js/tile.js")
 
 describe('tile', function(){
 	var newtile = null
+	var viewport = {}
+	viewport.src=""
+	viewport.sx=0
+	viewport.sy=0
+	viewport.width=0
+	viewport.height=0
+	viewport.x=0
+	viewport.y=0
+	viewport.drawImage = function(src,sx,sy,width,height,x,y){
+		viewport.src=src
+		viewport.sx=sx
+		viewport.sy=sy
+		viewport.width=width
+		viewport.height=height
+		viewport.x=x
+		viewport.y=y
+	}
 	beforeEach(function(){
 		testTile = new tile.Tile('#test',32,32,2,2,viewport)
 	})
 	describe('create', function(){
-		var viewport = {}
-		viewport.src=""
-		viewport.sx=0
-		viewport.sy=0
-		viewport.width=0
-		viewport.height=0
-		viewport.x=0
-		viewport.y=0
-		viewport.drawImage = function(src,sx,sy,width,height,x,y){
-			viewport.src=src
-			viewport.sx=sx
-			viewport.sy=sy
-			viewport.width=width
-			viewport.height=height
-			viewport.x=x
-			viewport.y=y
-		}
+		it('should hold values passed to it on creation', function(){
+			assert.equal('#test', testTile.src)
+			assert.equal(32, testTile.width)
+			assert.equal(32, testTile.height)
+			assert.equal(2, testTile.sheetX)
+			assert.equal(2, testTile.sheetY)
+		})
 		it('should call viewport.drawImage with it\'s settings when draw is called', function(){
 			testTile.draw(5,3)
 			assert.equal('#test',viewport.src)
